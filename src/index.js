4d8c813fdde48932bf8173d64f2352c35d32c03f
arllainm <arllain@gmail.com>
2020-02-16 12:33:44 -0300

    Config do ambiente de desenvolvimento, ESLint, Prettier, EditorConfig, Reactotron, React Navigation

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './config/ReactotronConfig';
+import Routes from './routes';
+
+console.tron.log('Arllain');
+export default function App() {
+  return <Routes />;
+}
